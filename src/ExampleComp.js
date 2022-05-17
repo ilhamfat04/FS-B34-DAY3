c9bf3cf9a7ce9f9e17b19a1c7ec93b23560e835a
= <=>
2022-05-17 10:50:04 +0700

    useContext

--- src/ExampleComp.js
+++ src/ExampleComp.js
@@ -1,17 +1,22 @@
 //import useEffect here
-import { useEffect } from "react"
+import { useEffect, useContext } from "react"
 import { Container } from "react-bootstrap";
 
-export default function ExampleComp(props) {
-    const { user } = props
+import { UserContext } from "./context/userContext";
 
-    console.log('Props example', user?.email);
+export default function ExampleComp() {
+    const [state, dispatch] = useContext(UserContext)
+    console.log(state);
+
+    // const { user } = props
+
+    // console.log('Props example', user?.email);
     // Create DidMount & WillUnmount with useEffect 
     // inside it can print "Guest Greeting Component Did Mount" & "Guest Greeting Component Will Unmount"
-    useEffect(() => {
-        console.log("Example comp did update");
-        console.log(user);
-    }, [props])
+    // useEffect(() => {
+    //     console.log("Example comp did update");
+    //     console.log(user);
+    // }, [props])
 
-    return <Container>Email : {props.user.email}</Container>
+    return <Container>Email : {state?.user?.email}</Container>
 }
